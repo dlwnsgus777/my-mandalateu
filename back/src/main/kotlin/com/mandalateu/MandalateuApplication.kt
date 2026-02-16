5c10c83163b6476c142ea5627f67c70d824402d8
이준현/E <finn@heyratel.com>
2026-02-16 23:01:31 +0900

    feat: 백엔드 프로젝트 생성

---
+++ back/src/main/kotlin/com/mandalateu/MandalateuApplication.kt
@@ -0,0 +1,11 @@
+package com.mandalateu
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MandalateuApplication
+
+fun main(args: Array<String>) {
+    runApplication<MandalateuApplication>(*args)
+}
---
+++ back/src/test/kotlin/com/mandalateu/MandalateuApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.mandalateu
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MandalateuApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
