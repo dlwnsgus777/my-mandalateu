deb3c0667f4e2ead7f3fde9549dd5f04bef8a6d3
dlwnsgus777 <dlwnsgus777@naver.com>
2026-02-18 22:23:31 +0900

    feat: back 4단계 테스트 수정

--- back/src/main/kotlin/com/mandalateu/common/config/SecurityConfig.kt
+++ back/src/main/kotlin/com/mandalateu/common/config/SecurityConfig.kt
@@ -4,12 +4,14 @@ import com.mandalateu.auth.jwt.JwtFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.HttpStatusEntryPoint
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
@@ -26,6 +28,9 @@ class SecurityConfig(
                 auth
                     .requestMatchers(HttpMethod.POST, "/api/v1/auth/**").permitAll()
                     .anyRequest().authenticated()
+            }
+            .exceptionHandling {
+                it.authenticationEntryPoint(HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
             }
             .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter::class.java)
         return http.build()
